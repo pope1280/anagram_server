1789e01029bb2b9a4cc9b7f75652d5068553bcca
Brian Pope <pope1280@gmail.com>
2013-05-21 00:18:57 -0700

    removed unnecessary table field from migration

--- db/migrate/20130520213435_create_words.rb
+++ db/migrate/20130520213435_create_words.rb
@@ -2,7 +2,6 @@ class CreateWords < ActiveRecord::Migration
   def change
     create_table  :words do |column|
       column.string  :word, :sorted_word
-      column.integer :word_length
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ File.open('words','r').each do |file|
   file.each_line do |word|
     Word.create(:word => word.chomp, 
                 :sorted_word => word.chomp.downcase.split(//).sort.join, 
-                :word_length => word.chomp.length)
+
   end
 end
